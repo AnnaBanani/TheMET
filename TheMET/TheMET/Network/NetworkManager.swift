8140613bf73963f176c58cf628b80ecb125b0840
Anna Sitnikova <annaasitnikovas@gmail.com>
2023-03-23 18:57:45 +0000

    Some mistakes fixed

--- TheMET/TheMET/Network/NetworkManager.swift
+++ TheMET/TheMET/Network/NetworkManager.swift
@@ -9,12 +9,12 @@ import Foundation
 
 class NetworkManager {
     
-    func loadData(urlString: String, parameters: [String : String], completion: @escaping([String: Any]?) -> Void)  {
+    func get(urlString: String, parameters: [String : String], completion: @escaping ([String: Any]?) -> Void)  {
         guard var components = URLComponents(string: urlString) else {
             completion(nil)
             return
         }
-        components.queryItems = parameters.map{ (key, value) in
+        components.queryItems = parameters.map { (key, value) in
             URLQueryItem(name: key, value: value)
         }
         components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
@@ -27,15 +27,14 @@ class NetworkManager {
         let urlSession = URLSession.shared
         let fileDownloadTask = urlSession.dataTask(
             with: urlRequest
-        ) { data, urlResponse, error in
+        ) { data, _, error in
             let executeCompletionOnMain: ([String: Any]?) -> Void = { loadedData in
                 DispatchQueue.main.async {
                     completion(loadedData)
                 }
             }
             guard error == nil,
-                  let data = data,
-                  let _ = urlResponse as? HTTPURLResponse else {
+                  let data = data else {
                 executeCompletionOnMain(nil)
                 return
             }
