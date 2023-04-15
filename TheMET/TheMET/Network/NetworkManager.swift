246a35a04612354a17ad9490feb38e7a2dcaa6f1
Anna Sitnikova <annaasitnikovas@gmail.com>
2023-04-15 13:41:23 +0300

    DispachQueue.asyncAfter method added

--- TheMET/TheMET/Network/NetworkManager.swift
+++ TheMET/TheMET/Network/NetworkManager.swift
@@ -10,38 +10,40 @@ import Foundation
 class NetworkManager {
     
     func get(urlString: String, parameters: [String : String], completion: @escaping ([String: Any]?) -> Void)  {
-        guard var components = URLComponents(string: urlString) else {
-            completion(nil)
-            return
-        }
-        components.queryItems = parameters.map { (key, value) in
-            URLQueryItem(name: key, value: value)
-        }
-        components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
-        guard let url = components.url else {
-            completion(nil)
-            return
-        }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "GET"
-        let urlSession = URLSession.shared
-        let fileDownloadTask = urlSession.dataTask(
-            with: urlRequest
-        ) { data, _, error in
-            let executeCompletionOnMain: ([String: Any]?) -> Void = { loadedData in
-                DispatchQueue.main.async {
-                    completion(loadedData)
-                }
-            }
-            guard error == nil,
-                  let data = data else {
-                executeCompletionOnMain(nil)
+        DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + .milliseconds(Int(12.5)), qos: .background) {
+            guard var components = URLComponents(string: urlString) else {
+                completion(nil)
                 return
             }
-            let result = try? JSONSerialization.jsonObject(with: data) as? [String: Any]
-            executeCompletionOnMain(result)
+            components.queryItems = parameters.map { (key, value) in
+                URLQueryItem(name: key, value: value)
+            }
+            components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+            guard let url = components.url else {
+                completion(nil)
+                return
+            }
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "GET"
+            let urlSession = URLSession.shared
+            let fileDownloadTask = urlSession.dataTask(
+                with: urlRequest
+            ) { data, _, error in
+                let executeCompletionOnMain: ([String: Any]?) -> Void = { loadedData in
+                    DispatchQueue.main.async {
+                        completion(loadedData)
+                    }
+                }
+                guard error == nil,
+                      let data = data else {
+                    executeCompletionOnMain(nil)
+                    return
+                }
+                let result = try? JSONSerialization.jsonObject(with: data) as? [String: Any]
+                executeCompletionOnMain(result)
+            }
+            fileDownloadTask.resume()
         }
-        fileDownloadTask.resume()
     }
     
 }
