70d7b2a388f8146ba2261388d4004b9fc79f4043
Anna Sitnikova <annaasitnikovas@gmail.com>
2023-04-18 14:54:01 +0300

    Hope I fixed everyting in thhe right way this time.

--- TheMET/TheMET/Network/NetworkManager.swift
+++ TheMET/TheMET/Network/NetworkManager.swift
@@ -9,62 +9,60 @@ import Foundation
 
 class NetworkManager {
     
+    private var lastRequestTime: Date = .now
+    
     func get(urlString: String, parameters: [String : String], completion: @escaping ([String: Any]?) -> Void)  {
-        var lastRequestTime = Date()
-            guard var components = URLComponents(string: urlString) else {
-                completion(nil)
-                return
-            }
-            components.queryItems = parameters.map { (key, value) in
-                URLQueryItem(name: key, value: value)
-            }
-            components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
-            guard let url = components.url else {
-                completion(nil)
-                return
-            }
-            var urlRequest = URLRequest(url: url)
-            urlRequest.httpMethod = "GET"
-            let urlSession = URLSession.shared
-            let fileDownloadTask = urlSession.dataTask(
-                with: urlRequest
-            ) { data, _, error in
-                let executeCompletionOnMain: ([String: Any]?) -> Void = { loadedData in
-                    DispatchQueue.main.async {
-                        completion(loadedData)
-                    }
-                }
-                guard error == nil,
-                      let data = data else {
-                    executeCompletionOnMain(nil)
-                    return
-                }
-                let result = try? JSONSerialization.jsonObject(with: data) as? [String: Any]
-                executeCompletionOnMain(result)
-            }
-        while !self.timeIntervalBWLastRequestTimeToNow(lastRequest: lastRequestTime) {
+        if self.isTimeIntervalLongEnough(lastRequest: lastRequestTime) {
+            self.executeNetworkGet(urlString: urlString, parameters: parameters, completion: completion)
+        } else {
             DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + .milliseconds(Int(12.5)), qos: .background) {
+                self.executeNetworkGet(urlString: urlString, parameters: parameters, completion: completion)
             }
         }
-        fileDownloadTask.resume()
         lastRequestTime = .now
     }
     
-   func timeIntervalBWLastRequestTimeToNow(lastRequest: Date) -> Bool {
-        let currentDate = Date()
-        let calendar = Calendar.current
-        let dateComponnentsFormatter = DateComponentsFormatter()
-        dateComponnentsFormatter.allowedUnits = [.second]
-        let differenceInSeconds = dateComponnentsFormatter.string(from: lastRequest, to: currentDate)
-        if let seconds = differenceInSeconds,
-           let secondsDouble = Double(seconds) {
-            return secondsDouble * 1000 > 12.5
+    private func executeNetworkGet(urlString: String, parameters: [String : String], completion: @escaping ([String: Any]?) -> Void)  {
+        guard var components = URLComponents(string: urlString) else {
+            completion(nil)
+            return
         }
-        else {
-            return false
+        components.queryItems = parameters.map { (key, value) in
+            URLQueryItem(name: key, value: value)
         }
+        components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+        guard let url = components.url else {
+            completion(nil)
+            return
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        let urlSession = URLSession.shared
+        let fileDownloadTask = urlSession.dataTask(
+            with: urlRequest
+        ) { data, _, error in
+            let executeCompletionOnMain: ([String: Any]?) -> Void = { loadedData in
+                DispatchQueue.main.async {
+                    completion(loadedData)
+                }
+            }
+            guard error == nil,
+                  let data = data else {
+                executeCompletionOnMain(nil)
+                return
+            }
+            let result = try? JSONSerialization.jsonObject(with: data) as? [String: Any]
+            executeCompletionOnMain(result)
+        }
+        fileDownloadTask.resume()
     }
-
+    
+    private func isTimeIntervalLongEnough(lastRequest: Date) -> Bool {
+        let currentDate = Date()
+        let differenceInSeconds = currentDate.distance(to: lastRequest)
+        let requiedTimeInterval: TimeInterval = 0.0125
+        return differenceInSeconds > requiedTimeInterval
+    }
 }
 
 
