f7a4885cdd8dd5b11d132bdf81e7a732e8820d22
Anna Sitnikova <annaasitnikovas@gmail.com>
2023-04-25 14:54:25 +0300

    Implementing FailedToLoadView

--- TheMET/TheMET/FailedToLoadView/FailedToLoadView.swift
+++ TheMET/TheMET/FailedToLoadView/FailedToLoadView.swift
@@ -1,39 +1,40 @@
 //
-//  FeaturedArtWorkLoadingView.swift
+//  FailedToLoadViewq.swift
 //  TheMET
 //
-//  Created by Анна Ситникова on 18/04/2023.
+//  Created by Анна Ситникова on 25/04/2023.
 //
 
 import Foundation
 import UIKit
 
-class FeaturedArtWorkLoadingView: UIView {
+class FailedToLoadView: UIView{
     
-    static let xibFileName = "FeaturedArtWorkLoadingView"
+    static let xibFileName = "FailedToLoadView"
     
     @IBOutlet var textLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var button: UIButton!
     
-    static func constructView() -> FeaturedArtWorkLoadingView {
-        let nib = UINib(nibName: FeaturedArtWorkLoadingView.xibFileName, bundle: nil)
-        return nib.instantiate(withOwner: nil).first as! FeaturedArtWorkLoadingView
-        }
+    static func constructView() -> FailedToLoadView {
+        let nib = UINib(nibName: FailedToLoadView.xibFileName, bundle: nil)
+        return nib.instantiate(withOwner: nil).first as! FailedToLoadView
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        textLabel.text = "Please wait while we choose a masterpiece"
+        textLabel.text = "Artwork cannot be upload \n \n \nMake sure you are connecting o Wi-Fi or your cellular network"
         textLabel.textColor = UIColor(red: 188.0/255, green: 176.0/255, blue: 193.0/255, alpha: 1)
-        imageView.image = UIImage(named: "Frame")
+        imageView.image = UIImage(named: "No Connection")
         button.backgroundColor = UIColor(red: 134.0/255, green: 121.0/255, blue: 139.0/255, alpha: 1)
         button.layer.cornerRadius = 30
         var buttonAttributed: [NSAttributedString.Key: Any] = [
             NSAttributedString.Key.foregroundColor: UIColor(red: 242.0/255, green: 242.0/255, blue: 242.0/255, alpha: 1)
         ]
-        let buttonTitle: String = "Generate new artwork"
+        let buttonTitle: String = "Try again"
         let attributedNexttButtonString = NSAttributedString(string: buttonTitle, attributes: buttonAttributed)
         button.setAttributedTitle(attributedNexttButtonString, for: .normal)
     }
+    
 }
 
