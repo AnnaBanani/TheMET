ebc79836a9269a1669d8c7c3cd8ed418173ddaa0
Anna Sitnikova <annaasitnikovas@gmail.com>
2023-05-26 12:49:39 +0100

    Implementing search method

--- TheMET/TheMET/Network/MetAPI.swift
+++ TheMET/TheMET/Network/MetAPI.swift
@@ -28,7 +28,7 @@ class MetAPI{
         if let date = metadataDate {
             dateString = dateFormatter.string(from: date)
             parameters["metadataDate"] = dateString
-        } 
+        }
         let idArrayString: String
         if !departmentIds.isEmpty {
             idArrayString = departmentIds.map {String($0)}.joined(separator: "|")
@@ -73,5 +73,74 @@ class MetAPI{
                 }
             }
     }
-
+    
+    func search(parameters: [SearchParameter], completion: @escaping (SearchResponse?) -> Void) {
+        var urlString: String = self.urlBaseString
+        let urlStringSuffix: String = "public/collection/v1/search"
+        urlString.append(urlStringSuffix)
+        var searchParameters: [String : String] = self.fillingParameters(parameters: parameters)
+        self.networkManager.get(
+            urlString: urlString,
+            parameters: searchParameters) { data in
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .useDefaultKeys
+                if let data = data {
+                    do {
+                        let result = try jsonDecoder.decode(SearchResponse.self, from: data)
+                        completion(result)
+                    } catch {
+                        completion(nil)
+                    }
+                } else {
+                    completion(nil)
+                }
+            }
+    }
+    
+    private func fillingParameters (parameters: [SearchParameter]) -> [String : String] {
+        var searchParameters: [String : String] = [ : ]
+        for parameter in parameters {
+            switch parameter {
+            case .q(let text):
+                searchParameters["q"] = text
+            case .isHighlight(let bool):
+                searchParameters["isHighlight"] = self.convertBoolToString(value: bool)
+            case .title(let bool):
+                searchParameters["title"] = self.convertBoolToString(value: bool)
+            case .tags(let bool):
+                searchParameters["tags"] = self.convertBoolToString(value: bool)
+            case .departmentId(let departmentId):
+                searchParameters["departmentId"] = String(departmentId)
+            case .isOnView(let bool):
+                searchParameters["isOnView"] = self.convertBoolToString(value: bool)
+            case .artistOrCulture(let bool):
+                searchParameters["artistOrCulture"] = self.convertBoolToString(value: bool)
+            case .medium(let medium):
+                searchParameters["medium"] = medium
+            case .hasImages(let bool):
+                searchParameters["hasImages"] = self.convertBoolToString(value: bool)
+            case .geoLocation(let geoLocation):
+                searchParameters["geoLocation"] = geoLocation
+            case .dates(let dates):
+                searchParameters["dates"] = self.covertDatasToString(from: dates.from, to: dates.to)
+            }
+        }
+        return searchParameters
+    }
+    
+    private func convertBoolToString(value: Bool) -> String {
+        if value {
+            return ("true")
+        } else {
+            return ("false")
+        }
+    }
+    
+    private func covertDatasToString(from: Int, to: Int) -> String {
+        var result: String
+        result = String(describing: from)
+        result.append("&")
+        result.append(String(describing: to))
+        return result
+    }
 }
