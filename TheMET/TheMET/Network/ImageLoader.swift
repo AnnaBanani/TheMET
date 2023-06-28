3e2c0bca7f5de519aab499156da2c6d37a981814
Anna Sitnikova <annaasitnikovas@gmail.com>
2023-06-28 16:25:19 +0100

    Introducing Cache in ImageLoader

--- TheMET/TheMET/Network/ImageLoader.swift
+++ TheMET/TheMET/Network/ImageLoader.swift
@@ -10,24 +10,33 @@ import UIKit
 
 class ImageLoader {
     
+    private var loadedImagesCollection: [URL : UIImage] = [:]
+    
     func loadImage(urlString: String, completion: @escaping (UIImage?) -> Void) {
+        
         guard let imageURL = URL(string: urlString) else {
             completion(nil)
             return
         }
-        DispatchQueue.global().async {
-            let executeComplitionOnMain: (UIImage?) -> Void = { image in
-                DispatchQueue.main.async {
-                    completion(image)
+        if self.loadedImagesCollection.keys.contains(imageURL) {
+            completion(self.loadedImagesCollection[imageURL])
+        } else {
+            DispatchQueue.global().async {
+                let executeComplitionOnMain: (UIImage?) -> Void = { image in
+                    DispatchQueue.main.async {
+                        self.loadedImagesCollection[imageURL] = image
+                        completion(image)
+                    }
+                }
+                if let imageData = try? Data(contentsOf: imageURL),
+                   let loadedImage = UIImage(data: imageData) {
+                    executeComplitionOnMain(loadedImage)
+                } else {
+                   executeComplitionOnMain(nil)
                 }
             }
-            if let imageData = try? Data(contentsOf: imageURL),
-               let loadedImage = UIImage(data: imageData) {
-                executeComplitionOnMain(loadedImage)
-            } else {
-               executeComplitionOnMain(nil)
-            }
         }
+        
     }
 }
 
