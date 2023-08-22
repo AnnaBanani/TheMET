d26f62d1f857a25a7c4e521383e57e0b415bfaa8
Anna Sitnikova <annaasitnikovas@gmail.com>
2023-08-22 15:42:55 +0100

    Fixing Navigation bar back button

--- TheMET/TheMET/Extentions/UINavigationItem+apply.swift
+++ TheMET/TheMET/Extentions/UINavigationItem+apply.swift
@@ -9,18 +9,28 @@ import Foundation
 import UIKit
     
 extension UINavigationItem {
+    
     func apply(title: String, color: UIColor?, fontName: String, fontSize: Double) -> UINavigationBarAppearance{
         self.title = title
         let navigationBarAppearance = UINavigationBarAppearance()
         navigationBarAppearance.configureWithOpaqueBackground()
         navigationBarAppearance.backgroundColor = .clear
+        let backItemAppearance = UIBarButtonItemAppearance()
+        var image = UIImage()
         if let color: UIColor = color,
-           let font: UIFont = UIFont(name: fontName, size: fontSize) {
+           let font: UIFont = UIFont(name: fontName, size: fontSize),
+           let chevronImage = UIImage(systemName: "chevron.backward"){
             navigationBarAppearance.titleTextAttributes = [
                 NSAttributedString.Key.foregroundColor: color,
                 NSAttributedString.Key.font: font
             ]
+            backItemAppearance.normal.titleTextAttributes = [.foregroundColor : color]
+            image = chevronImage.withTintColor(color, renderingMode: .alwaysOriginal)
         }
+        navigationBarAppearance.setBackIndicatorImage(image, transitionMaskImage: image)
+        navigationBarAppearance.backButtonAppearance = backItemAppearance
         return navigationBarAppearance
     }
 }
+
+
