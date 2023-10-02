9b192e617445e2e345d4111a3d41a4e3e776598a
Andrei Simvolokov <andrei.simvolokov@team.bumble.com>
2023-10-02 15:04:38 +0100

    MetAPI framework introduced

---
+++ TheMET/TheMET/MetAPI/MetAPI/TestFile.swift
@@ -0,0 +1,12 @@
+//
+//  TestFile.swift
+//  MetAPI
+//
+//  Created by Andrei Simvolokov on 02/10/2023.
+//
+
+import Foundation
+
+// TODO: This is a placeholder. Remove this file.
+class TestClass {
+}
