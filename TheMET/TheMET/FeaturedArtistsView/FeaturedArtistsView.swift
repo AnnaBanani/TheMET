d8b3da5cd145ea7ac2817525df7545eac40bc16c
Anna Sitnikova <annaasitnikovas@gmail.com>
2023-11-02 16:17:00 +0000

    Implementing new section view

--- TheMET/TheMET/FeaturedArtistsView/FeaturedArtistsView.swift
+++ TheMET/TheMET/FeaturedArtistsView/FeaturedArtistsView.swift
@@ -14,11 +14,13 @@ class FeaturedArtistsView: UIView {
     private let titleLabel: UILabel = UILabel()
     private let gradientView: VerticalGradientView = VerticalGradientView()
     
+    var onViewTapped: (() -> Void)?
+    
     var title: String? {
         get { return self.titleLabel.attributedText?.string }
         set {
             if let newValue = newValue {
-                self.titleLabel.apply(font: NSLocalizedString("serif_font", comment: ""), color: UIColor(named: "pear"), fontSize: 18, title: newValue)
+                self.titleLabel.apply(font: NSLocalizedString("san_serif_font_bold", comment: ""), color: UIColor(named: "pear"), fontSize: 18, title: newValue)
             } else {
                 self.titleLabel.attributedText = nil
             }
@@ -32,6 +34,8 @@ class FeaturedArtistsView: UIView {
         self.gradientView.translatesAutoresizingMaskIntoConstraints = false
         self.backgroundColor = UIColor(named: "blueberry")
         self.imageView.image = UIImage(named: "FeaturedArtistsCover")
+        self.imageView.contentMode = .scaleAspectFill
+        self.imageView.clipsToBounds = true
         self.addSubview(self.imageView)
         self.addSubview(self.titleLabel)
         self.addSubview(self.gradientView)
@@ -48,14 +52,42 @@ class FeaturedArtistsView: UIView {
             self.gradientView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
         NSLayoutConstraint.activate([
-            self.titleLabel.leadingAnchor.constraint(equalTo: self.imageView.leadingAnchor, constant: 8),
-            self.titleLabel.trailingAnchor.constraint(equalTo: self.imageView.trailingAnchor, constant: -8),
-            self.titleLabel.bottomAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: -8)
+            self.titleLabel.centerXAnchor.constraint(equalTo: self.imageView.centerXAnchor),
+            self.titleLabel.bottomAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: -16)
         ])
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapView(_:)))
+        self.addGestureRecognizer(tapGestureRecognizer)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    @objc
+    private func didTapView(_ sender: UITapGestureRecognizer) {
+        self.onViewTapped?()
+    }
+    
+    private func highlightView() {
+        self.imageView.alpha = 0.8
+    }
+
+    private func dehighlightView() {
+        self.imageView.alpha = 1
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.highlightView()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        self.dehighlightView()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        self.dehighlightView()
+    }
 }
