42f8cb68b426ac9c3f6c200835903090472f2a31
Anna Sitnikova <annaasitnikovas@gmail.com>
2023-11-07 17:47:58 +0000

    Implementing new view and cell for artists catalog

---
+++ TheMET/TheMET/FeaturedArtistsContentView/FeaturedArtistsCell.swift
@@ -0,0 +1,125 @@
+//
+//  FeaturedArtistsCell.swift
+//  TheMET
+//
+//  Created by Анна Ситникова on 07/11/2023.
+//
+
+import Foundation
+import UIKit
+
+class FeaturedArtistsCell: UICollectionViewCell {
+    
+    private let imageView: LoadingImageView = LoadingImageView()
+    private let cardsTitleLabel: UILabel = UILabel()
+    private let cardsSubTitleLabel: UILabel = UILabel()
+    private let gradientView: VerticalGradientView = VerticalGradientView()
+    
+    var isPlaceholderVisible: Bool = false {
+        didSet {
+            if self.isPlaceholderVisible {
+                self.imageView.isHidden = true
+                self.cardsTitleLabel.isHidden = true
+                self.cardsSubTitleLabel.isHidden = true
+            } else {
+                self.imageView.isHidden = false
+                self.cardsTitleLabel.isHidden = false
+                self.cardsSubTitleLabel.isHidden = false
+            }
+        }
+    }
+    
+    var title: String? {
+        get {
+            return self.cardsTitleLabel.attributedText?.string
+        }
+        set {
+            self.reapplyTitleStyle(text: newValue)
+        }
+    }
+    
+    var subtitle: String? {
+        get {
+            return self.cardsSubTitleLabel.text
+        }
+        set {
+            self.reapplySubTitleStyle(text: newValue)
+        }
+    }
+    
+    var backgroundState: LoadingImageView.State {
+        get {
+            return self.imageView.state
+        }
+        set {
+            self.imageView.state = newValue
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        self.imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.gradientView.translatesAutoresizingMaskIntoConstraints = false
+        self.cardsTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.cardsSubTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = UIColor(named: "blueberry")
+        self.contentView.addSubview(self.imageView)
+        self.contentView.addSubview(self.gradientView)
+        self.contentView.addSubview(self.cardsTitleLabel)
+        self.contentView.addSubview(self.cardsSubTitleLabel)
+        
+        NSLayoutConstraint.activate([
+            self.imageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            self.imageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            self.imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            self.gradientView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            self.gradientView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            self.gradientView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.gradientView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            self.cardsTitleLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 8),
+            self.cardsTitleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -8),
+            self.cardsTitleLabel.bottomAnchor.constraint(equalTo: self.cardsSubTitleLabel.topAnchor, constant: -4)
+        ])
+        NSLayoutConstraint.activate([
+            self.cardsSubTitleLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 8),
+            self.cardsSubTitleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -8),
+            self.cardsSubTitleLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -8)
+        ])
+    }
+    
+    private func reapplyTitleStyle(text: String?) {
+        guard let text = text else {
+            self.cardsTitleLabel.attributedText = nil
+            return
+        }
+        self.cardsTitleLabel.apply(font: NSLocalizedString("san_serif_font_bold", comment: ""), color: UIColor(named: "pear"), fontSize: 14, title: text)
+    }
+    
+    private func reapplySubTitleStyle(text: String?) {
+        guard let text = text else {
+            self.cardsSubTitleLabel.attributedText = nil
+            return
+        }
+        self.cardsSubTitleLabel.apply(font: NSLocalizedString("san_serif_font", comment: ""), color: UIColor(named: "pear"), fontSize: 12, title: text)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.title = nil
+        self.subtitle = nil
+        self.backgroundState = .loading
+    }
+}
---
+++ TheMET/TheMET/FeaturedArtistsContentView/FeaturedArtistsContentView.swift
@@ -0,0 +1,100 @@
+//
+//  FeaturedArtistsContentView.swift
+//  TheMET
+//
+//  Created by Анна Ситникова on 07/11/2023.
+//
+
+import Foundation
+import UIKit
+
+class FeaturedArtistsContentView: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var content: [FeaturedArtistsCellData] = [] {
+        didSet {
+            self.collectionView.reloadData()
+        }
+    }
+    private let imageLoader = ImageLoader()
+    
+    var onFeaturedArtistsCellTap: (_ artistID: Int) -> Void = { _ in }
+    
+    static let xibFileName = "FeaturedArtistsContentView"
+    static let cellIdentifier = "CatalogCell"
+    
+    var onFeaturedArtistsCellWillDisplay: (_ artistID: Int) -> Void = { _ in }
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    var flowLayout: UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: self.bounds.width * 0.5, height: 180)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 10
+        return layout
+    }
+    
+    static func constructView() -> FeaturedArtistsContentView {
+        let nib = UINib(nibName: FeaturedArtistsContentView.xibFileName, bundle: nil)
+        return nib.instantiate(withOwner: nil).first as! FeaturedArtistsContentView
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.collectionView.register(CatalogCell.self, forCellWithReuseIdentifier: FeaturedArtistsContentView.cellIdentifier)
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.collectionView.collectionViewLayout = self.flowLayout
+    }
+    
+    // UICollectionViewDelegate
+    // UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.content.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FeaturedArtistsContentView.cellIdentifier, for: indexPath) as? CatalogCell {
+            let cellContent = self.content[indexPath.row]
+            cell.tag = cellContent.artistId
+            switch cellContent.artistData {
+            case .placeholder:
+                cell.isPlaceholderVisible = true
+            case .data(let imageURL, let title, let subTitle):
+                cell.isPlaceholderVisible = false
+                cell.title = title
+                cell.subtitle = subTitle
+                cell.backgroundState = .loading
+                if let imageURL = imageURL {
+                    self.imageLoader.loadImage(urlString: imageURL.absoluteString) { image in
+                        guard cell.tag == cellContent.artistId else { return }
+                        if let image = image {
+                            cell.backgroundState = .loaded(image)
+                        } else {
+                            cell.backgroundState = .failed
+                        }
+                    }
+                } else {
+                    cell.backgroundState = .failed
+                }
+            }
+            return cell
+        } else {
+            print ("Error")
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.collectionView.deselectItem(at: indexPath, animated: true)
+        let cellData = self.content[indexPath.row]
+        self.onFeaturedArtistsCellTap(cellData.artistId)
+    }
+  
+    func collectionView(_: UICollectionView, willDisplay: UICollectionViewCell, forItemAt: IndexPath) {
+        let cellData = self.content[forItemAt.row]
+        self.onFeaturedArtistsCellWillDisplay(cellData.artistId)
+    }
+}
