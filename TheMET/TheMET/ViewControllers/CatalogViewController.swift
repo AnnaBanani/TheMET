d94aea141924ead0ff18732307d81d442110a93f
Anna Sitnikova <annaasitnikovas@gmail.com>
2024-01-09 15:10:58 +0000

    scrollView contentGuide fixed

--- TheMET/TheMET/ViewControllers/CatalogViewController.swift
+++ TheMET/TheMET/ViewControllers/CatalogViewController.swift
@@ -16,8 +16,6 @@ class CatalogViewController: UIViewController {
     
     private let scrollView: UIScrollView = UIScrollView()
     
-    private let scrollContainer: UIView = UIView()
-    
     private let departmentsSectionViewController: DepartmentsSectionViewController = DepartmentsSectionViewController()
     
     private let featuredArtistsViewController: ArtistsSectionViewController = ArtistsSectionViewController()
@@ -52,35 +50,28 @@ class CatalogViewController: UIViewController {
         ])
         let contentGuide = self.scrollView.contentLayoutGuide
         let frameGuide = self.scrollView.frameLayoutGuide
-        self.scrollContainer.translatesAutoresizingMaskIntoConstraints = false
-        self.scrollView.addSubview(self.scrollContainer)
         NSLayoutConstraint.activate([
-            self.scrollContainer.leadingAnchor.constraint(equalTo: contentGuide.leadingAnchor),
-            self.scrollContainer.trailingAnchor.constraint(equalTo: contentGuide.trailingAnchor),
-            self.scrollContainer.bottomAnchor.constraint(equalTo: contentGuide.bottomAnchor),
-            self.scrollContainer.topAnchor.constraint(equalTo: contentGuide.topAnchor),
-            self.scrollContainer.leadingAnchor.constraint(equalTo: frameGuide.leadingAnchor),
-            self.scrollContainer.trailingAnchor.constraint(equalTo: frameGuide.trailingAnchor),
-            self.scrollContainer.heightAnchor.constraint(equalToConstant: 800)
+            contentGuide.leadingAnchor.constraint(equalTo: frameGuide.leadingAnchor),
+            contentGuide.trailingAnchor.constraint(equalTo: frameGuide.trailingAnchor)
         ])
         
         self.navigationItem.rightBarButtonItem = aboutButton
         self.departmentsSectionViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        self.scrollContainer.addSubview(self.departmentsSectionViewController.view)
+        self.scrollView.addSubview(self.departmentsSectionViewController.view)
         NSLayoutConstraint.activate([
-            self.departmentsSectionViewController.view.leadingAnchor.constraint(equalTo: self.scrollContainer.leadingAnchor),
-            self.departmentsSectionViewController.view.trailingAnchor.constraint(equalTo: self.scrollContainer.trailingAnchor),
+            self.departmentsSectionViewController.view.leadingAnchor.constraint(equalTo: contentGuide.leadingAnchor),
+            self.departmentsSectionViewController.view.trailingAnchor.constraint(equalTo: contentGuide.trailingAnchor),
             self.departmentsSectionViewController.view.heightAnchor.constraint(equalToConstant: 240),
-            self.departmentsSectionViewController.view.topAnchor.constraint(equalTo: self.scrollContainer.topAnchor)
+            self.departmentsSectionViewController.view.topAnchor.constraint(equalTo: contentGuide.topAnchor)
         ])
         self.addChild(self.departmentsSectionViewController)
         self.departmentsSectionViewController.didMove(toParent: self)
         self.featuredArtistsViewController.view.translatesAutoresizingMaskIntoConstraints = false
         
-        self.scrollContainer.addSubview(self.featuredArtistsViewController.view)
+        self.scrollView.addSubview(self.featuredArtistsViewController.view)
         NSLayoutConstraint.activate([
-            self.featuredArtistsViewController.view.leadingAnchor.constraint(equalTo: self.scrollContainer.leadingAnchor),
-            self.featuredArtistsViewController.view.trailingAnchor.constraint(equalTo: self.scrollContainer.trailingAnchor),
+            self.featuredArtistsViewController.view.leadingAnchor.constraint(equalTo: contentGuide.leadingAnchor),
+            self.featuredArtistsViewController.view.trailingAnchor.constraint(equalTo: contentGuide.trailingAnchor),
             self.featuredArtistsViewController.view.heightAnchor.constraint(equalToConstant: 240),
             self.featuredArtistsViewController.view.topAnchor.constraint(equalTo: self.departmentsSectionViewController.view.bottomAnchor)
         ])
@@ -88,12 +79,13 @@ class CatalogViewController: UIViewController {
         self.featuredArtistsViewController.didMove(toParent: self)
         self.culturesSectionViewController.view.translatesAutoresizingMaskIntoConstraints = false
         
-        self.scrollContainer.addSubview(self.culturesSectionViewController.view)
+        self.scrollView.addSubview(self.culturesSectionViewController.view)
         NSLayoutConstraint.activate([
-            self.culturesSectionViewController.view.leadingAnchor.constraint(equalTo: self.scrollContainer.leadingAnchor),
-            self.culturesSectionViewController.view.trailingAnchor.constraint(equalTo: self.scrollContainer.trailingAnchor),
+            self.culturesSectionViewController.view.leadingAnchor.constraint(equalTo: contentGuide.leadingAnchor),
+            self.culturesSectionViewController.view.trailingAnchor.constraint(equalTo: contentGuide.trailingAnchor),
             self.culturesSectionViewController.view.heightAnchor.constraint(equalToConstant: 240),
-            self.culturesSectionViewController.view.topAnchor.constraint(equalTo: self.featuredArtistsViewController.view.bottomAnchor)
+            self.culturesSectionViewController.view.topAnchor.constraint(equalTo: self.featuredArtistsViewController.view.bottomAnchor),
+            self.culturesSectionViewController.view.bottomAnchor.constraint(equalTo: contentGuide.bottomAnchor)
         ])
         self.addChild(self.culturesSectionViewController)
         self.culturesSectionViewController.didMove(toParent: self)
