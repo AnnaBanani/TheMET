a6bd0f444b54d224d36fc9913b95451bd2f01572
Anna Sitnikova <annaasitnikovas@gmail.com>
2024-02-05 16:48:09 +0000

    Improving Map Expirience

--- TheMET/TheMET/ViewModels/AboutMETViewModel.swift
+++ TheMET/TheMET/ViewModels/AboutMETViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Combine
 import UIKit
+import MapKit
 
 class AboutMETViewModel {
     
@@ -45,13 +46,70 @@ class AboutMETViewModel {
     }
     
     func topLocatonViewDidTap() {
-        print ("top")
+        self.locationViewDidTap(latitude: self.topMapViewData.latitude, longitude: self.topMapViewData.longitude, name: NSLocalizedString("about_met_the_met_fifth_avenue_title_lable", comment: ""), address: "The+Metropolitan+Museum+of+Art,+1000+Fifth+Avenue+New+York,+NY+10028")
     }
     
     func bottomLocatonViewDidTap() {
-        print ("bottom")
+        self.locationViewDidTap(latitude: self.bottomMapViewData.latitude, longitude: self.bottomMapViewData.longitude, name: NSLocalizedString("about_met_the_met_cloisters_title_lable", comment: ""), address: "The+Met+Cloisters,+99+Margaret+Corbin+Drive+Fort+Tryon+Park+New+York,+NY+10040")
+    }
+    
+    private func locationViewDidTap(latitude: Double, longitude: Double, name: String, address: String) {
+        guard let presentingController = self.presentingControllerProvider() else {
+            return
+        }
+        if UIApplication.shared.canOpenURL(NSURL(string:"comgooglemaps://")! as URL) {
+            let alertController = UIAlertController(
+                title: nil,
+                message: nil,
+                preferredStyle: .actionSheet
+            )
+            let googleAction = UIAlertAction(
+                title: "Show in Google Maps",
+                style: .default,
+                handler: { _ in
+                    UIApplication.shared.open(
+                        URL(
+                            string:
+                                "comgooglemaps://?q=\(address)&center=\(latitude),\(longitude)&zoom=14"
+                        )!
+                    )
+                }
+            )
+            let appleMapAction = UIAlertAction(
+                title: "Show in Apple Maps",
+                style: .default
+            ) { _ in
+                self.openInAppleMap(latitude: latitude, longitude: longitude, name: name)
+            }
+            let cancelAction = UIAlertAction(
+                title: "Cancel",
+                style: .cancel
+            )
+            alertController.addAction(googleAction)
+            alertController.addAction(appleMapAction)
+            alertController.addAction(cancelAction)
+            presentingController.present(alertController, animated: true)
+        } else {
+            self.openInAppleMap(latitude: latitude, longitude: longitude, name: name)
+        }
+    }
+    
+    private func openInAppleMap(latitude: Double, longitude: Double, name:  String) {
+        let regionDistance:CLLocationDistance = 10000
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let regionSpan = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        let options: [String : Any] = [
+            MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),
+            MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)
+        ]
+        let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = name
+        mapItem.openInMaps(launchOptions: options)
     }
 }
+
+
 
 private extension AboutMETViewModel.MapViewData {
     static let fifthAvenue = AboutMETViewModel.MapViewData(
